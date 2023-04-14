c84aa676340bf1f12b704d8d9495c3609d5a4bfd
Drew Harms <Drew.Harms@vu.com>
2023-04-14 12:47:16 -0500

    count

--- displayInventory.py
+++ displayInventory.py
@@ -3,6 +3,16 @@ stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 def displayInventory(inventory):
     # your code goes here
 
+    total_inventory = 0
+
+    print(f'Inventory:')
+
+    for aaa,bbb in inventory.items() :
+        print(bbb,aaa)
+        total_inventory += bbb
+
+    print(f'Total number of items: {total_inventory}')
+
 
 if __name__ == "__main__":
     displayInventory(stuff)
